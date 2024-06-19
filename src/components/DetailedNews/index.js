d3909bbf61cb0f0892f1d7b9999bf60c637389d6
Ganesh7386 <puttaganesh7386@gmail.com>
2024-06-19 15:04:54 +0530

    first commit

--- src/components/DetailedNews/index.js
+++ src/components/DetailedNews/index.js
@@ -1,3 +1,12 @@
-const DetailedNews = ()=> (<h1>This is detailed news component</h1>)
+import {useLocation} from 'react-router-dom'
 
+
+const DetailedNews = ()=> {
+
+    const location = useLocation();
+    console.log(location);
+    
+    return (<h1>This is detailed news component</h1>)
+
+}
 export default DetailedNews;
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,6 +1,12 @@
+import {Link} from 'react-router-dom'
+
+
 function Home() {
     return (
-        <h1>This is home</h1>
+        <><h1>This is home</h1>
+        <Link to = "/detailednews" state = {{name : "ganesh"}} >Go to each</Link>
+        </>
+        
     )
 }
 
