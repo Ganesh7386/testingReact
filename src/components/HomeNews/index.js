4af0e28ae74dac1328afc2cbec940eb5fdbaec55
Ganesh7386 <puttaganesh7386@gmail.com>
2024-06-19 18:38:52 +0530

    fourth commit

--- src/components/HomeNews/index.js
+++ src/components/HomeNews/index.js
@@ -1,8 +1,8 @@
 import './index.css'
-import {useState,useContext, useEffect} from 'react';
+import {useState,useContext, useEffect , useMemo} from 'react';
 // import EachNewsCard from '../EachNewsCard/index'
 import {SearchAndCategoryContext} from '../SearchCategoryContext/index'
-// import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from 'uuid';
 
 function HomeNews() {
     const inputCategoryChangeContext = useContext(SearchAndCategoryContext)
@@ -14,15 +14,23 @@ function HomeNews() {
             const url = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=69abcad3b3794c36bde8b80de589167b`;
             const fetchingPromise = await fetch(url);
             const actualNewsData = await fetchingPromise.json();
-            setNewsList(actualNewsData.articles);
+            const listOfArticles = actualNewsData.articles;
+            // console.log(listOfArticles)
+
+            const articlesWithId = listOfArticles.map(article => ({
+                id: uuidv4(),
+                ...article
+            }));
+            setNewsList(articlesWithId);
         }
         fetchNewsData();
     } , [searchValue , category])
 
     const filterNullData = useMemo(()=> {
         const filteredList = newsList.filter((eachNews)=>(eachNews.content !== null || eachNews.description !== null));
-        const filtered1 = newsList.map((eachNews)=>({newsId : uuidv4() , ...eachNews}))
-        console.log(filtered1);
+        console.log(filteredList);
+
+        return filteredList;
         
         // console.log(filteredList);
     },[newsList])
@@ -30,11 +38,11 @@ function HomeNews() {
     return (
     <div className = "HomeNewsContainer">
         <h1>This is Home news container</h1>
-        {/* <ul className = "newsArticlesUlContainer">
+        <ul className = "newsArticlesUlContainer">
             {
-                newsList.map((eachNewsobj)=>(<li key = {eachNewsobj.newsId}>{eachNewsobj.author}</li>))
+                newsList.map((eachNewsobj)=>(<li key = {eachNewsobj.id}>{eachNewsobj.author}</li>))
             }
-        </ul> */}
+        </ul>
     </div>
     )
 }
